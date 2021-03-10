6dfc726e562916c8da0f42d684a869773912cbf6
Mohamed Shafee <mshafee1993@gmail.com>
2021-03-10 08:30:00 -0500

    replace the join event with a createRoom and joinRoom event

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ const PORT = 3005
 
 io.on('connect', socket => {
   console.log("A user has connected")
-  
-  socket.on('join', ({gameId}) => { 
-    socket.join(gameId)
-    console.log("User has joined room")
-  })
 
+  socket.on('createRoom', ({gameId}) => { 
+    console.log("Game host has opened room.")
+    socket.join(gameId);
+  });
+  
+  socket.on('joinRoom', ({gameId}) => { 
+    console.log("Opponent has joined the hosts room.")
+    socket.join(gameId);
+    io.sockets.in(gameId).emit('opponentConnected', {})
+  });
+  
   socket.on('movePiece', ({ gameId, sourceSquare, targetSquare }) => {
     console.log("Player moved")
     io.sockets.in(gameId).emit('opponentMoved', {sourceSquare: sourceSquare, targetSquare:targetSquare })
   })
+
 })
  
 http.listen(PORT, () => {
